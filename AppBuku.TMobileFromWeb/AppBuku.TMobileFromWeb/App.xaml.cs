075b30a73085d6b5626c88afc42a223833255021
imamfarisi19 <edwardindo1@gmail.com>
2021-05-19 06:58:39 +0700

    first commit

---
+++ AppBuku.TMobileFromWeb/AppBuku.TMobileFromWeb/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using AppBuku.TMobileFromWeb.Services;
+using AppBuku.TMobileFromWeb.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBuku.TMobileFromWeb
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            // Set properties untuk aplikasi 
+            //this.Properties.Add("BaseWebUri", "http://appbuku.somee.com/");
+            //this.Properties.Add("WebUsername", "u006imam");
+            //this.Properties.Add("WebPassword", "u006ima-@aqw67");
+
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
